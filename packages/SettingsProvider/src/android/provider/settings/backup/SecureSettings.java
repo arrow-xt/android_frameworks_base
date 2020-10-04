d04a4e6566024c755991351a4affa05c1b1e5f2e
jhonboy121 <alfredmathew05@gmail.com>
2020-10-04 12:49:28 -0700

    SystemUI: allow changing the length of gesture navbar [1/2]
    * forward ported from 11 and improved a lot by delegating all the dirty work to controllers and using LayoutParams for changing the width instead of screwing around in onDraw.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Ibdf88461a489b84fb73fa304cd0b73e0fbef163c
    
    -----
    
    SystemUI: Update additional navbar dimens for Android 13
    
    The gesture navigation bar is now more prominent in Android 13 and navigation_home_handle_width increased to 108dp.
    Update the steps in the navbar length seekbar to reflect this.
    
    Change-Id: Ic019afe5608560e5df4ecde651b2e18747d16a37
    Co-authored-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -245,5 +245,6 @@ public class SecureSettings {
         Settings.Secure.SEARCH_LONG_PRESS_HOME_ENABLED,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.BERRY_BLACK_THEME,
+        Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
     };
 }
