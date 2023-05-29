77f2958c66a09ce92c3cca0cb469ca0e5c70f064
Matt Filetto <matt.filetto@gmail.com>
2023-05-29 02:33:00 -0700

    QSTiles: Allow changing tile label size independently.
    * Adds a secondary label size setting

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -105,6 +105,7 @@ public class SystemSettings {
         Settings.System.QS_TILE_VERTICAL_LAYOUT,
         Settings.System.QS_LAYOUT,
         Settings.System.QS_TILE_LABEL_SIZE,
+        Settings.System.QS_TILE_SECONDARY_LABEL_SIZE,
         Settings.System.QQS_LAYOUT,
     };
 }
