Pyrtle93/alpha_fwb

QS header images: backup and validate settings

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,6 +89,16 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.MAX_VISIBLE_NOTIFICATION_ICONS,
         Settings.System.QS_PANEL_STYLE,
-        Settings.System.QS_HAPTIC_FEEDBACK
+        Settings.System.QS_HAPTIC_FEEDBACK,
+        Settings.System.QS_HEADER_IMAGE,
+        Settings.System.QS_HEADER_IMAGE_TINT,
+        Settings.System.QS_HEADER_IMAGE_TINT_CUSTOM,
+        Settings.System.QS_HEADER_IMAGE_ALPHA,
+        Settings.System.QS_HEADER_IMAGE_HEIGHT_PORTRAIT,
+        Settings.System.QS_HEADER_IMAGE_HEIGHT_LANDSCAPE,
+        Settings.System.QS_HEADER_IMAGE_LANDSCAPE_ENABLED,
+        Settings.System.QS_HEADER_IMAGE_PADDING_SIDE,
+        Settings.System.QS_HEADER_IMAGE_PADDING_TOP,
+        Settings.System.QS_HEADER_IMAGE_URI
     };
 }

arrow-xt/android_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -107,5 +107,15 @@ public class SystemSettings {
         Settings.System.QS_TILE_LABEL_SIZE,
         Settings.System.QS_TILE_SECONDARY_LABEL_SIZE,
         Settings.System.QQS_LAYOUT,
+        Settings.System.QS_HEADER_IMAGE,
+        Settings.System.QS_HEADER_IMAGE_TINT,
+        Settings.System.QS_HEADER_IMAGE_TINT_CUSTOM,
+        Settings.System.QS_HEADER_IMAGE_ALPHA,
+        Settings.System.QS_HEADER_IMAGE_HEIGHT_PORTRAIT,
+        Settings.System.QS_HEADER_IMAGE_HEIGHT_LANDSCAPE,
+        Settings.System.QS_HEADER_IMAGE_LANDSCAPE_ENABLED,
+        Settings.System.QS_HEADER_IMAGE_PADDING_SIDE,
+        Settings.System.QS_HEADER_IMAGE_PADDING_TOP,
+        Settings.System.QS_HEADER_IMAGE_URI,
     };
 }